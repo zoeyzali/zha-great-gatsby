4a64a0a252408426cd90a15d7e8008387c3d4b43
zoeyzali <zoeyzali@gmail.com>
2020-01-06 18:30:41 +0100

    navbar-layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import './bootstrap.min.css'
 import "./layout.css"
 
 const Layout = ( { children } ) => <>{children}</>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@ import React from "react"
 // import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import SpeakingPage from "./speaking"
+import Navbar from "../components/Globals/Navbar"
+import { FaGulp } from 'react-icons/fa'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Navbar />
     <h1>Coffee People</h1>
+    <FaGulp />
     <p>Welcome to your new Gatsby site.</p>
-    <SpeakingPage />
   </Layout>
 )
 
