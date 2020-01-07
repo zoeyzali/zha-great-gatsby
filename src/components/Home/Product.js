51a78d474c98e14df8b60759e290018aa85f9b3c
zoeyzali <zoeyzali@gmail.com>
2020-01-07 22:44:59 +0100

    trying out webhooks

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -6,7 +6,6 @@ const Product = ( { product } ) => {
     return (
         <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-2">
             <div className="card" style={{ minHeight: "100%" }}>
-                <div style={{ maxHeight: "400px" }}></div>
                 <Img fluid={product.image.fluid} className="card-img-top" />
                 <div className="card-body">
                     <h6 className="text-capitalize">{product.title}</h6>
